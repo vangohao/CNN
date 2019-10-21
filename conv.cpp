170a491f733e8929ae51278896624029ccd69481
Vango Hao <vangohao@gmail.com>
2019-10-21 12:16:58 +0800

    baseline

--- conv.cpp
+++ conv.cpp
@@ -12,9 +12,13 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	Parameter:  CHin|CHout|R|C|K|S
 	*/
 #pragma HLS INTERFACE s_axilite port = return
-#pragma HLS INTERFACE m_axi depth = 24576 port = In offset = slave //adjust the depth as you need
-#pragma HLS INTERFACE m_axi depth = 14400 port = Out offset = slave
-#pragma HLS INTERFACE m_axi depth = 3456 port = W offset = slave
+// #pragma HLS INTERFACE m_axi depth = 24576 port = In offset = slave //adjust the depth as you need
+// #pragma HLS INTERFACE m_axi depth = 14400 port = Out offset = slave
+// #pragma HLS INTERFACE m_axi depth = 3456 port = W offset = slave
+// #pragma HLS INTERFACE m_axi depth = 256 port = Parameter offset = slave
+#pragma HLS INTERFACE m_axi depth = 60000 port = In offset = slave //adjust the depth as you need
+#pragma HLS INTERFACE m_axi depth = 60000 port = Out offset = slave
+#pragma HLS INTERFACE m_axi depth = 60000 port = W offset = slave
 #pragma HLS INTERFACE m_axi depth = 256 port = Parameter offset = slave
 
 	// 当前block size :
@@ -35,13 +39,13 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	const int KMax = 5;
 	const int SMin = 1;
 
-	// 为方便起见, 在block计算时默认增加Padding, 并且默认Stride=1, 即bR_out=bR_in, 算完后抛弃多余的.
+	// 为方便起见, 在block计算时默认增加Padding, 即bR_out=bR_in, 不过Out_1中只有部分会被使用, 算完后抛弃多余的.
 	const int bR_out = bR_in;
 	const int bC_out = bC_in;
 
-	d_type In_1[bCHin * bR_in * bC_in];
+	d_type In_1[bR_in * bC_in * bCHin];
 	d_type Out_1[bR_out * bC_out * bCHout];
-	d_type W_1[bCHin * KMax * KMax * bCHout];
+	d_type W_1[KMax * KMax * bCHin * bCHout];
 #pragma HLS ARRAY_PARTITION variable = In_1 cyclic factor = 1 dim = 1
 #pragma HLS ARRAY_PARTITION variable = Out_1 cyclic factor = 16 dim = 1
 #pragma HLS ARRAY_PARTITION variable = W_1 cyclic factor = 16 dim = 1
@@ -77,7 +81,7 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	int vbR_out = ((bR_in - K) / S) + 1;
 	int vbC_out = ((bC_in - K) / S) + 1;
 	// int C_out = 30;
-	for (int i = 0; i < CHout * R_out * C_out; i ++)
+	for (int i = 0; i < CHout * R_out * C_out; i++)
 	{
 #pragma HLS PIPELINE
 		Out[i] = 0;
@@ -87,30 +91,30 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	{
 		for (int CHout_batch = 0; CHout_batch < CHout; CHout_batch += bCHout)
 		{
-			for (int R_in_batch = 0, R_out_batch = 0; R_in_batch < R_in ; (R_in_batch += vbR_in), (R_out_batch += vbR_out))
+			for (int R_in_batch = 0, R_out_batch = 0; R_in_batch < R_in; (R_in_batch += vbR_in), (R_out_batch += vbR_out))
 			{
-				for (int C_in_batch = 0, C_out_batch = 0; C_in_batch < C_in ; (C_in_batch += vbC_in), (C_out_batch += vbC_out))
+				for (int C_in_batch = 0, C_out_batch = 0; C_in_batch < C_in; (C_in_batch += vbC_in), (C_out_batch += vbC_out))
 				{
 					printf("FUCKYOU! %d %d %d %d\n", CHin_batch, CHout_batch, R_in_batch, C_in_batch);
 #pragma HLS LOOP_FLATTEN OFF
 
+				loop_W:
 					for (int i = 0; i < bCHout && i + CHout_batch < CHout; i++)
 					{
 						for (int j = 0; j < bCHin && j + CHin_batch < CHin; j++)
 						{
 							for (int k = 0; k < K; k++)
 							{
-								for(int l =  0; l < K; l++)
-						{
+								for (int l = 0; l < K; l++)
+								{
 #pragma HLS PIPELINE
-								
-							W_1[j * K * K * bCHout + k * K * bCHout + l * bCHout + i] = W[(i + CHout_batch) * (CHin * K * K) + (j + CHin_batch) * K * K + k * K + l];
-						}
-							}
 
+									W_1[k * K * bCHin * bCHout + l * bCHin * bCHout + j * bCHout + i] = W[(i + CHout_batch) * (CHin * K * K) + (j + CHin_batch) * K * K + k * K + l];
+								}
+							}
 						}
 					}
-
+				loop_In:
 					for (int i = 0; i < bCHin && i + CHin_batch < CHin; i++)
 					{
 						for (int j = 0; j < bR_in && j + R_in_batch < R_in; j++)
@@ -118,15 +122,22 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 							for (int k = 0; k < bC_in && k + C_in_batch < C_in; k++)
 							{
 #pragma HLS PIPELINE
-								In_1[i * bR_in * bC_in + j * bC_in + k] = In[(i + CHin_batch) * (bR_in * bC_in) + (j + R_in_batch) * bC_in + (k + C_in_batch)];
+								In_1[j * bC_in * bCHin + k * bCHin + i] = In[(i + CHin_batch) * (bR_in * bC_in) + (j + R_in_batch) * bC_in + (k + C_in_batch)];
 							}
 						}
 					}
-
-					for (int i = 0; i < bR_out * bC_out * bCHout; i++)
+				loop_Out:
+					for (int r2 = 0; r2 < vbR_out && r2 + R_out_batch < R_out; r2++)
 					{
+						for (int c2 = 0; c2 < vbC_out && c2 + C_out_batch < C_out; c2++)
+						{
+							for (int cho = 0; cho < bCHout && cho + CHout_batch < CHout; cho++)
+							{
 #pragma HLS PIPELINE
-						Out_1[i] = 0;
+								// #pragma HLS UNROLL
+								Out_1[cho + (r2) * (bC_out * bCHout) + (c2) * (bCHout)] = Out[(cho + CHout_batch) * R_out * C_out + (r2 + R_out_batch) * C_out + (c2 + C_out_batch)];
+							}
+						}
 					}
 
 				loop_Kr:
@@ -146,20 +157,21 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 								loop_C1:
 									for (int c1 = 0; c1 < bC_in; c1++)
 									{
+// #pragma HLS UNROLL factor = 4
 #pragma HLS PIPELINE
 									loop_CHout:
 										for (int cho = 0; cho < bCHout; cho++)
 										{
-#pragma HLS UNROLL factor = 16
-											Out_1[(cho) + r1 * bC_out * bCHout + c1 * bCHout] += W_1[(cho) + chi * (bCHout * K * K) + kr * (bCHout * K) + kc * (bCHout)] * In_1[chi * (bR_in * bC_in) + (S * r1 + kr) * bC_in + (S * c1 + kc)];
+#pragma HLS UNROLL
+											Out_1[(cho) + r1 * bC_out * bCHout + c1 * bCHout] += W_1[(cho) + chi * (bCHout) + kr * (bCHin * bCHout * K) + kc * (bCHin * bCHout)] * In_1[chi + (S * r1 + kr) * bC_in * bCHin + (S * c1 + kc) * bCHin];
 										}
 									}
 								}
 							}
 						}
 					}
-					
-				loop_Add:
+
+				loop_AddedOut:
 					for (int r2 = 0; r2 < vbR_out && r2 + R_out_batch < R_out; r2++)
 					{
 						for (int c2 = 0; c2 < vbC_out && c2 + C_out_batch < C_out; c2++)
@@ -167,8 +179,7 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 							for (int cho = 0; cho < bCHout && cho + CHout_batch < CHout; cho++)
 							{
 #pragma HLS PIPELINE
-// #pragma HLS UNROLL
-								Out[(cho + CHout_batch) * R_out * C_out + (r2 + R_out_batch) * C_out + (c2 + C_out_batch)] += Out_1[cho + (r2) * (bC_out * bCHout) + (c2) * (bCHout)];
+								Out[(cho + CHout_batch) * R_out * C_out + (r2 + R_out_batch) * C_out + (c2 + C_out_batch)] = Out_1[cho + (r2) * (bC_out * bCHout) + (c2) * (bCHout)];
 							}
 						}
 					}
