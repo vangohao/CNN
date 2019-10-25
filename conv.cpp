66425b842f638ab5b26d3dde36b558e83967ad6b
Vango Hao <vangohao@gmail.com>
2019-10-25 19:31:08 +0800

    unroll*2

--- conv.cpp
+++ conv.cpp
@@ -3,8 +3,8 @@
 #include <stdio.h>
 void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 {
-#pragma HLS ALLOCATION instances = fmul limit = 16 operation
-#pragma HLS ALLOCATION instances = fadd limit = 16 operation
+#pragma HLS ALLOCATION instances = fmul limit = 32 operation
+#pragma HLS ALLOCATION instances = fadd limit = 32 operation
 
 /*
 	In  : Input feature map, CHin*R*C
@@ -47,9 +47,10 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	d_type In_1[bR_in][bC_in][bCHin];
 	d_type Out_1[bR_out][bC_out][bCHout];
 	d_type W_1[KMax][KMax][bCHin][bCHout];
-#pragma HLS ARRAY_PARTITION variable = In_1 cyclic factor = 1 dim = 3
+#pragma HLS ARRAY_PARTITION variable = In_1 cyclic factor = 4 dim = 2
 #pragma HLS ARRAY_PARTITION variable = Out_1 cyclic factor = 16 dim = 3
-#pragma HLS ARRAY_PARTITION variable = W_1 cyclic factor = 16 dim = 4
+#pragma HLS ARRAY_PARTITION variable = Out_1 cyclic factor=2 dim = 2
+#pragma HLS ARRAY_PARTITION variable = W_1 complete
 	// #pragma HLS ARRAY_PARTITION variable=W_1 complete
 
 	ap_uint<8> CHin, CHout, R_in, C_in;
@@ -163,7 +164,7 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 								loop_C1:
 									for (ap_uint<8> c1 = 0; c1 < bC_in; c1++)
 									{
-// #pragma HLS UNROLL factor = 4
+#pragma HLS UNROLL factor = 2
 #pragma HLS PIPELINE
 									loop_CHout:
 										for (ap_uint<8> cho = 0; cho < bCHout; cho++)
