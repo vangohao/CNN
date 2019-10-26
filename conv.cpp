3ffad7c54bac697d71fb3850b11050d71d4a1b77
Vango Hao <vangohao@gmail.com>
2019-10-26 19:35:07 +0800

    pingpong

--- conv.cpp
+++ conv.cpp
@@ -1,6 +1,89 @@
 #include "conv.h"
 #include <ap_int.h>
 #include <stdio.h>
+const unsigned int bCHout = 32;
+const unsigned int bCHin = 16;
+const unsigned int bR_in = 32;
+const unsigned int bC_in = 32;
+const unsigned int KMax = 5;
+const unsigned int SMin = 1;
+
+// 为方便起见, 在block计算时默认增加Padding, 即bR_out=bR_in, 不过Out_1中只有部分会被使用, 算完后抛弃多余的.
+const unsigned int bR_out = bR_in;
+const unsigned int bC_out = bC_in;
+ap_uint<8> CHin, CHout, R_in, C_in;
+ap_uint<4> K;
+ap_uint<1> S;
+void load_w(d_type *W, d_type W_1[KMax][KMax][bCHin][bCHout],ap_uint<8> CHout_batch,ap_uint<8> CHin_batch, unsigned offset)
+{
+	loop_W:
+	for (ap_uint<8> i = 0; i < bCHout && i + CHout_batch < CHout; i++)
+	{
+		for (ap_uint<8> j = 0; j < bCHin && j + CHin_batch < CHin; j++)
+		{
+			for (ap_uint<4> k = 0; k < K; k++)
+			{
+	#pragma HLS LOOP_TRIPCOUNT min=3 max=3
+				for (ap_uint<4> l = 0; l < K; l++)
+				{
+	#pragma HLS LOOP_TRIPCOUNT min=3 max=3
+	#pragma HLS PIPELINE
+
+					W_1[k][l][j][i] = W[offset + i * (CHin * K * K) + j * K * K + k * K + l];
+				}
+			}
+		}
+	}
+}
+void load_in(d_type *In, d_type In_1[bR_in][bC_in][bCHin],ap_uint<8> R_in_batch, ap_uint<8>C_in_batch, ap_uint<8>CHin_batch, unsigned offset)
+{
+loop_In:
+	for (ap_uint<8> j = 0; j < bR_in && j + R_in_batch < R_in; j++)
+	{
+		for (ap_uint<8> k = 0; k < bC_in && k + C_in_batch < C_in; k++)
+		{
+			for (ap_uint<8> i = 0; i < bCHin && i + CHin_batch < CHin; i++)
+			{
+#pragma HLS PIPELINE
+				In_1[j][k][i] = In[offset + i * (R_in * C_in) + j * C_in + k];
+			}
+		}
+	}
+}
+void conv_batch(d_type In_1[bR_in][bC_in][bCHin],d_type Out_1[bR_out][bC_out][bCHout]
+			,d_type W_1[KMax][KMax][bCHin][bCHout], ap_int<8> CHin_batch)
+{
+loop_Kr:
+	for (ap_uint<4> kr = 0; kr < K; kr++)
+	{
+	#pragma HLS LOOP_TRIPCOUNT min = 2 max = 5
+	loop_Kc:
+		for (ap_uint<4> kc = 0; kc < K; kc++)
+		{
+	#pragma HLS LOOP_TRIPCOUNT min = 2 max = 5
+		loop_CHin:
+			for (ap_uint<8> chi = 0; chi < bCHin && chi + CHin_batch < CHin; chi++)
+			{
+			loop_R1:
+				for (ap_uint<8> r1 = 0; r1 < bR_in; r1++)
+				{
+				loop_C1:
+					for (ap_uint<8> c1 = 0; c1 < bC_in; c1++)
+					{
+	// #pragma HLS UNROLL factor = 2
+	#pragma HLS PIPELINE
+					loop_CHout:
+						for (ap_uint<8> cho = 0; cho < bCHout; cho++)
+						{
+	#pragma HLS UNROLL
+							Out_1[r1][c1][cho] += W_1[kr][kc][chi][cho] * In_1[(r1 << S) + kr][(c1 << S) + kc][chi];
+						}
+					}
+				}
+			}
+		}
+}
+}
 void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 {
 #pragma HLS ALLOCATION instances = fmul limit = 32 operation
@@ -33,29 +116,20 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 	K : 2, 3, 5
 	S : 1, 2
 	*/
-	const unsigned int bCHout = 32;
-	const unsigned int bCHin = 32;
-	const unsigned int bR_in = 32;
-	const unsigned int bC_in = 32;
-	const unsigned int KMax = 5;
-	const unsigned int SMin = 1;
-
-	// 为方便起见, 在block计算时默认增加Padding, 即bR_out=bR_in, 不过Out_1中只有部分会被使用, 算完后抛弃多余的.
-	const unsigned int bR_out = bR_in;
-	const unsigned int bC_out = bC_in;
 
 	d_type In_1[bR_in][bC_in][bCHin];
 	d_type Out_1[bR_out][bC_out][bCHout];
+	d_type In_0[bR_in][bC_in][bCHin];
+	d_type W_0[KMax][KMax][bCHin][bCHout];
 	d_type W_1[KMax][KMax][bCHin][bCHout];
+// #pragma HLS RESOURCE variable=Out_1 core=RAM_1P_LUTRAM
 // #pragma HLS ARRAY_PARTITION variable = In_1 cyclic factor = 4 dim = 2
 #pragma HLS ARRAY_PARTITION variable = Out_1 cyclic factor = 32 dim = 3
 // #pragma HLS ARRAY_PARTITION variable = Out_1 complete
 #pragma HLS ARRAY_PARTITION variable = W_1 cyclic factor= 32 dim=4
+#pragma HLS ARRAY_PARTITION variable = W_0 cyclic factor= 32 dim=4
 	// #pragma HLS ARRAY_PARTITION variable=W_1 complete
 
-	ap_uint<8> CHin, CHout, R_in, C_in;
-	ap_uint<4> K;
-	ap_uint<1> S;
 	/*
 	CHin : Input channels
 	CHout : output channels
@@ -112,74 +186,30 @@ void cnn(d_type *In, d_type *Out, d_type *W, int *Parameter)
 						}
 					}
 				}
-				for (ap_uint<8> CHin_batch = 0; CHin_batch < CHin; CHin_batch += bCHin)
+				bool ping_pong_flag = 1;
+				for (ap_uint<8> CHin_batch = 0; CHin_batch < CHin + bCHin /* ping pong add 1 */; CHin_batch += bCHin)
 				{
 #pragma HLS LOOP_TRIPCOUNT max=1
 					printf("FUCKYOU! %u %u %u %u\n", (unsigned)CHin_batch, (unsigned)CHout_batch, (unsigned)R_in_batch, (unsigned)C_in_batch);
 // #pragma HLS LOOP_FLATTEN OFF
 
-				loop_W:
-					for (ap_uint<8> i = 0; i < bCHout && i + CHout_batch < CHout; i++)
-					{
-						for (ap_uint<8> j = 0; j < bCHin && j + CHin_batch < CHin; j++)
-						{
-							for (ap_uint<4> k = 0; k < K; k++)
-							{
-#pragma HLS LOOP_TRIPCOUNT min=3 max=3
-								for (ap_uint<4> l = 0; l < K; l++)
-								{
-#pragma HLS LOOP_TRIPCOUNT min=3 max=3
-#pragma HLS PIPELINE
+					unsigned w_offset = CHout_batch * (CHin * K * K) + CHin_batch * (K * K);
+					unsigned in_offset = CHin_batch * (R_in * C_in) + R_in_batch * C_in + C_in_batch;
 
-									W_1[k][l][j][i] = W[(i + CHout_batch) * (CHin * K * K) + (j + CHin_batch) * K * K + k * K + l];
-								}
-							}
-						}
-					}
-				loop_In:
-					for (ap_uint<8> j = 0; j < bR_in && j + R_in_batch < R_in; j++)
+					if (ping_pong_flag)
 					{
-						for (ap_uint<8> k = 0; k < bC_in && k + C_in_batch < C_in; k++)
-						{
-							for (ap_uint<8> i = 0; i < bCHin && i + CHin_batch < CHin; i++)
-							{
-#pragma HLS PIPELINE
-								In_1[j][k][i] = In[(i + CHin_batch) * (R_in * C_in) + (j + R_in_batch) * C_in + (k + C_in_batch)];
-							}
-						}
+						load_w(W, W_1, CHout_batch, CHin_batch, w_offset);
+						load_in(In, In_1, R_in_batch, C_in_batch, CHin_batch, in_offset);
+						conv_batch(In_0, Out_1, W_0, CHin_batch);
+					}
+					else
+					{
+						load_w(W, W_0, CHout_batch, CHin_batch, w_offset);
+						load_in(In, In_0, R_in_batch, C_in_batch, CHin_batch, in_offset);
+						conv_batch(In_1, Out_1, W_1, CHin_batch);
 					}
 
-				loop_Kr:
-					for (ap_uint<4> kr = 0; kr < K; kr++)
-					{
-#pragma HLS LOOP_TRIPCOUNT min = 2 max = 5
-					loop_Kc:
-						for (ap_uint<4> kc = 0; kc < K; kc++)
-						{
-#pragma HLS LOOP_TRIPCOUNT min = 2 max = 5
-						loop_CHin:
-							for (ap_uint<8> chi = 0; chi < bCHin && chi + CHin_batch < CHin; chi++)
-							{
-							loop_R1:
-								for (ap_uint<8> r1 = 0; r1 < bR_in; r1++)
-								{
-								loop_C1:
-									for (ap_uint<8> c1 = 0; c1 < bC_in; c1++)
-									{
-// #pragma HLS UNROLL factor = 2
-#pragma HLS PIPELINE
-									loop_CHout:
-										for (ap_uint<8> cho = 0; cho < bCHout; cho++)
-										{
-#pragma HLS UNROLL
-											Out_1[r1][c1][cho] += W_1[kr][kc][chi][cho] * In_1[(r1 << S) + kr][(c1 << S) + kc][chi];
-										}
-									}
-								}
-							}
-						}
-					}
-
+					ping_pong_flag = !ping_pong_flag;
 				}
 			loop_AddedOut:
 				for (ap_uint<8> r2 = 0; r2 < vbR_out && r2 + R_out_batch < R_out; r2++)
--- run_hls.tcl
+++ run_hls.tcl
@@ -1,14 +1,14 @@
-open_project -reset CNN
+open_project CNN
 set_top cnn
 add_files conv.cpp
 add_files conv.h
 add_files conv_testbench.cpp -tb
-open_solution -reset "solution1"
+open_solution "solution1"
 set_part {xc7z020clg400-1} -tool vivado
-create_clock -period 10 -name default
+create_clock -period 6 -name default
 
 # source "./solution1/directives.tcl"
-csim_design
+#csim_design
 csynth_design
 #cosim_design -trace_level all -tool xsim
 export_design -format ip_catalog
